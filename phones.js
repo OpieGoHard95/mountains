637405e563337ef55423eb0a77728331b6712ab6
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-27 12:14:48 -0500

    Added my dataset to be tested

---
+++ phones.js
@@ -0,0 +1,13 @@
+var PHONES = [
+  {name: "iPhone6", company: "Apple", price: 300},
+  {name: "iPhone5c", company: "Apple", price: 100}, 
+  {name: "Galaxy s5", company: "Samsung", price: 200},
+  {name: "Galaxy s4", company: "Samsung", price: 100},
+  {name: "Note Edge", company: "Samsung", price: 400},
+
+
+
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = PHONES;
