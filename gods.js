a042d5ee64008108e6c307c2cd9f7a68eb4a3e7f
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-20 11:24:15 -0400

    Created a file for the CMP project

---
+++ gods.js
@@ -0,0 +1,14 @@
+var GODS = [
+  {name: "agni", favor: 1250, release: 1338436800000},
+  {name: "ullr", favor: 5500, release: 1395201600000},
+  {name: "athena", favor: 5500, release: 1370404800000},
+  {name: "thor", favor: 0, release: 1346990400000},
+  {name: "serqet", favor: 5500, release: 1405396800000},
+  {name: "mercury", favor: 5500, release: 1380686400000},
+  {name: "bellona", favor: 11000, release: 1424840400000},
+  {name: "nox", favor: 5500, release: 1414555200000}
+];
+
+if (typeof module != "undefined" && module.exports)
+
+  module.exports = GODS;
