d4200133f720c607bc39ad4a09c46b07fbebd645
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-20 11:41:04 -0400

    First big attempt at the problem, took advice from larder

---
+++ dateChecker.js
@@ -0,0 +1,36 @@
+load('gods.js');
+var gods = JSON.parse(GODS_FILE);
+
+function filter(array, test) {
+  var passed = [];
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i]))
+      passed.push(array[i]);
+  }
+  return passed;
+}
+
+// Attempting find gods created within the last year
+
+print(JSON.stringify(filter(gods, function(name) {
+	return name.release > 1357016400000 ;
+})));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
